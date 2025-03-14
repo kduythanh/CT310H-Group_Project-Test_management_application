9bcf9608685234c7541aa5916804ce8df82d2438
Dung Ho <hoducdung3103@gmail.com>
2025-03-14 15:41:05 +0700

    Add frmXemKetQua

---
+++ TestManagementApp/frmXemKetQua.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TestManagementApp
+{
+    partial class frmXemKetQua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboMaDeThi = new System.Windows.Forms.ComboBox();
+            this.btnXemKetQua = new System.Windows.Forms.Button();
+            this.dgvKetQua = new System.Windows.Forms.DataGridView();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetQua)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(266, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã đề thi:";
+            
+            // 
+            // cboMaDeThi
+            // 
+            this.cboMaDeThi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboMaDeThi.FormattingEnabled = true;
+            this.cboMaDeThi.Location = new System.Drawing.Point(370, 29);
+            this.cboMaDeThi.Name = "cboMaDeThi";
+            this.cboMaDeThi.Size = new System.Drawing.Size(121, 30);
+            this.cboMaDeThi.TabIndex = 1;
+            this.cboMaDeThi.SelectedIndexChanged += new System.EventHandler(this.cboMaDeThi_SelectedIndexChanged);
+            // 
+            // btnXemKetQua
+            // 
+            this.btnXemKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXemKetQua.Location = new System.Drawing.Point(576, 29);
+            this.btnXemKetQua.Name = "btnXemKetQua";
+            this.btnXemKetQua.Size = new System.Drawing.Size(135, 32);
+            this.btnXemKetQua.TabIndex = 2;
+            this.btnXemKetQua.Text = "Xem kết quả";
+            this.btnXemKetQua.UseVisualStyleBackColor = true;
+            this.btnXemKetQua.Click += new System.EventHandler(this.btnXemKetQua_Click);
+            // 
+            // dgvKetQua
+            // 
+            this.dgvKetQua.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKetQua.Location = new System.Drawing.Point(67, 76);
+            this.dgvKetQua.Name = "dgvKetQua";
+            this.dgvKetQua.RowHeadersWidth = 51;
+            this.dgvKetQua.RowTemplate.Height = 24;
+            this.dgvKetQua.Size = new System.Drawing.Size(664, 289);
+            this.dgvKetQua.TabIndex = 3;
+            this.dgvKetQua.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKetQua_CellContentClick);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(346, 389);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(107, 35);
+            this.btnThoat.TabIndex = 4;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // frmXemKetQua
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.dgvKetQua);
+            this.Controls.Add(this.btnXemKetQua);
+            this.Controls.Add(this.cboMaDeThi);
+            this.Controls.Add(this.label1);
+            this.Name = "frmXemKetQua";
+            this.Text = "frmXemKetQua";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetQua)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboMaDeThi;
+        private System.Windows.Forms.Button btnXemKetQua;
+        private System.Windows.Forms.DataGridView dgvKetQua;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
---
+++ TestManagementApp/frmXemKetQua.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestManagementApp
+{
+    public partial class frmXemKetQua : Form
+    {
+        public frmXemKetQua()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnXemKetQua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboMaDeThi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnXemKetQua.Enabled = cboMaDeThi.SelectedIndex != -1; // Chỉ bật nút khi chọn mã đề
+        }
+
+        private void dgvKetQua_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
